573c3e6db8257a1f99d0ca75a09b2a9a30fe23ae
Ihsaan7 <ihsaan2215@gmail.com>
2025-09-14 13:25:22 +0500

    HomeProdcutSet

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import React from "react";
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Hero from "./components/home/Hero";
-import Container from "./components/Container";
-import RecentProducts from "./components/RecentProducts";
-import FeaturedProducts from "./components/FeaturedProducts";
+import Products from "./components/home/Products";
 
 function App() {
   return (
     <div className="flex flex-col">
         <Header />
         <Hero />
-        <RecentProducts />
-        <FeaturedProducts />
+        <Products />
         <Footer />
     </div>
     
