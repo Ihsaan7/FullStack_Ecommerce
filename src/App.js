936df219a6a9cea942bc8ca605370254fd9cde16
Ihsaan7 <ihsaan2215@gmail.com>
2025-10-03 08:53:42 +0500

    Old_Routing_React_Ecommerce_Uncomplete

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
 import React from "react";
-import Header from "./components/Header"
-import Footer from "./components/Footer"
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Hero from "./components/home/Hero";
 import Products from "./components/home/Products";
-import { BrowserRouter, Route , Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Store from "./pages/Store";
 import Cart from "./pages/Cart";
-
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="flex flex-col">
+      <div className="flex flex-col justify-normal">
         <Header />
         <Routes>
-          <Route path="/" element=
-          {
+          <Route
+            path="/"
+            element={
               <>
-              <Hero />
-              <Products />
+                <Hero />
+                <Products />
               </>
-          }/>
-          <Route path="/store" element={<Store />}/>
-          <Route path="/cart" element={<Cart />}/>
+            }
+          />
+          <Route path="/store" element={<Store />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
         <Footer />
-    </div>
+      </div>
     </BrowserRouter>
-    
-)}
+  );
+}
 
 export default App;
